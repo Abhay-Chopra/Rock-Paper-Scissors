76e765d82e826317d2f7ec8ce5f76a40992f19c7
Abhay Chopra <abhayc.908@gmail.com>
2022-07-08 03:19:30 -0600

    Add game loop and logic, add helper functions to handle interaction
    Updated README.md to include tasks that require immediate attention,
    i.e. sanitizing input and fixing the game loop such that it doesn't
    break

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+game();
 function random(start, end) {
   return Math.floor(Math.random() * (end - start + 1) + start);
 }
@@ -6,10 +9,65 @@ function computerPlay() {
   let option = random(0, 2);
   switch (option) {
     case 0:
-      return 'Rock';
+      return 'rock';
     case 1:
-      return 'Paper';
+      return 'paper';
     case 2:
-      return 'Scissors';
+      return 'scissors';
+  }
+}
+
+function capitalize(inputString) {
+  inputString = String(inputString);
+  inputString =
+    inputString[0].toUpperCase() + inputString.slice(1).toLowerCase();
+  return inputString;
+}
+
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case 'rock':
+      if (computerSelection == 'rock') {
+        return "You Tied! Rock can't beat Rock!";
+      } else if (computerSelection == 'paper') {
+        computerScore++;
+        return 'You Lose! Paper beats Rock!';
+      } else if (computerSelection == 'scissors') {
+        playerScore++;
+        return 'You Won! Rock beats Scissors!';
+      }
+    case 'paper':
+      if (computerSelection == 'rock') {
+        playerScore++;
+        return 'You Win! Paper beats Rock!';
+      } else if (computerSelection == 'paper') {
+        return "You Tied! Paper can't beat Paper!";
+      } else if (computerSelection == 'scissors') {
+        computerScore++;
+        return 'You Lose! Scissors beats Paper!';
+      }
+    case 'scissors':
+      if (computerSelection == 'rock') {
+        computerScore++;
+        return 'You Lose! Rock beats Paper!';
+      } else if (computerSelection == 'paper') {
+        playerScore++;
+        return 'You Win! Scissor beats Paper!';
+      } else if (computerSelection == 'scissors') {
+        return "You Tied! Scissors can't beat Scissors!";
+      }
+  }
+}
+
+function game() {
+  while (!(playerScore >= 5 || computerScore >= 5)) {
+    let playerSelection = prompt('Pick from Rock Paper Scissors...');
+    //Quitting Option
+    if (playerSelection == 'quit') {
+      break;
+    }
+    let computerSelection = computerPlay();
+    alert(`The computer picked ... ${capitalize(computerSelection)}!`);
+    alert(playRound(playerSelection, computerSelection));
   }
 }
